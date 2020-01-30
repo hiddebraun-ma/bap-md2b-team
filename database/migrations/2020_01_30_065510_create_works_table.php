ceded6272069a5a73ca146fe0be48abe9c01d250
Hidde Braun <hidde.braun@gmail.com>
2020-01-30 08:02:53 +0100

    first commit

---
+++ database/migrations/2020_01_30_065510_create_works_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->url('title');
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->bigInteger('student_id');
+            $table->timestamps();
+
+            $table->foreign('student_id')->references('id')->on('student');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
